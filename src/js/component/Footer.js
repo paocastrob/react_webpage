569ba94c00233f9d04742c1998fad0e94458becb
paocastrob <paocastrob@gmail.com>
2019-07-19 20:57:22 +0000

    map the cards

---
+++ src/js/component/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//create your first component
+export default class CardDeck extends React.Component {
+	render() {
+		return (
+			<div className="footer bg-dark">
+				<h4>Copyright © Your Website 2019</h4>
+			</div>
+		);
+	}
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,29 +1,18 @@
 import React from "react";
 import { Nav } from "./Nav"; //Nav's file uses "export class Nav extends React.Component", so we need the "{}"
 import Jumbotron from "./Jumbotron"; //Jumbotron's file uses "export default class Jumbotron extends React.Component", so we don't need the "{ }"
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import CardDeck from "./CardDeck"; //Jumbotron's file uses "export default class Jumbotron extends React.Component", so we don't need the "{ }"
+import Footer from "./Footer"; //Jumbotron's file uses "export default class Jumbotron extends React.Component", so we don't need the "{ }"
 
 //create your first component
 export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>
-					This is the Home Page, and and we are importing other files
-					to organize our content.
-				</h1>
-				<h2>Below we are using the Nav Info file with</h2>
 				<Nav />
-				<h2>Below we are using the Jumbotron file:</h2>
 				<Jumbotron />
-				<h2>
-					Next step is to duplicate a file in the component folder,
-					such as Nav, and update the export inormation (think in
-					terms of what you want that collection of information to be
-					referred to when it is needed)
-				</h2>
+				<CardDeck />
+				<Footer />
 			</div>
 		);
 	}
